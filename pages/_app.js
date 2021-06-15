65404dbbdd9811256051b60768db1e621d95a774
Audric Ganser <audricganser@gmail.com>
2021-06-15 09:55:58 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
       <Component {...pageProps} />
-      <Footer />
     </div>
   )
 }
